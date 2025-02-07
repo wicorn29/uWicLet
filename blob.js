5093c3e7116b21962c8121598e85adc153835bfe
Wilson Corn <wilson.corn@hotmail.com>
2025-02-07 11:25:49 -0800

    Update blob.js

--- blob.js
+++ blob.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     if (window.self !== window.top && document.title === "uWicLet") {
         return;
     }
-    if ((window.location.hostname.endsWith('.google.com') && window.location.hostname !== 'www.google.com') || window.location.hostname == 'www.youtube.com') {
+    if ((window.location.hostname.endsWith('.googlef.com') && window.location.hostname !== 'www.googlef.com') || window.location.hostname == 'www.youtubef.com') {
         window.addEventListener('keydown', function (event) {
             if (event.code == "Backquote" && event.ctrlKey && event.shiftKey) {
                 window.alert("uWicLet failed to load on this page! Error: Google Subdomain");
