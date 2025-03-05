3cbfcb621b5f23b540817159e0853bb87092f0f1
Wilson Corn <wilson.corn@hotmail.com>
2025-03-05 12:02:02 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import { openBlobFrame, closeIframe } from './iframe.js';
-import { handleMessage } from './messageHandler.js';
-import { startDragging, stopDragging, drag } from './drag.js';
-import { openSettings } from './settings.js';
+import { openBlobFrame, closeIframe } from 'https://raw.githubusercontent.com/wicorn29/uWicLet/refs/heads/main/CoreJS/iframe.js';
+import { handleMessage } from 'https://raw.githubusercontent.com/wicorn29/uWicLet/refs/heads/main/CoreJS/messageHandler.js';
+import { startDragging, stopDragging, drag } from 'https://raw.githubusercontent.com/wicorn29/uWicLet/refs/heads/main/CoreJS/drag.js';
+import { openSettings } from 'https://raw.githubusercontent.com/wicorn29/uWicLet/refs/heads/main/CoreJS/settings.js';
 
 try {
     document.addEventListener("keydown", function (blob) {
