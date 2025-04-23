9fb7e498264c0699d273269697a55b8a6e7b849d
Wilson Corn <wilson.corn@hotmail.com>
2025-04-23 12:23:38 -0700

    Implemented new UI options

--- blob.js
+++ blob.js
@@ -1,4 +1,4 @@
-/// fetch('https://raw.githubusercontent.com/wicorn29/uWicLet/refs/heads/main/dev/devinit.js').then(response => response.text()).then(scriptContent => { eval(scriptContent); }); // load in the debugger
+fetch('https://raw.githubusercontent.com/wicorn29/uWicLet/refs/heads/main/dev/devinit.js').then(response => response.text()).then(scriptContent => { eval(scriptContent); }); // load in the debugger
 
 /// blob.js
 
