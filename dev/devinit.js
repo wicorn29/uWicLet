4c899d78328de0935f4278833528e9f333d61a21
Wilson Corn <wilson.corn@hotmail.com>
2025-04-23 10:39:25 -0700

    Update devinit.js

--- dev/devinit.js
+++ dev/devinit.js
@@ -6,7 +6,7 @@ document.addEventListener("keydown", function (e) {
 		(e.src = "//wicorn29.github.io/uWicLet/dev/devinit.html"),
 			(e.style.cssText = "width:100%; height:100%; border:none;"),
 			t.document.body.appendChild(e),
-			t.document.title = "uRun",
+			t.document.title = "CHROMEBREW EXECUTED PROGRAM",
 			t.addEventListener("message", function (e) {
 				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
 			});
