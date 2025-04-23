e46ec4b6f98891892345584ebfcd5d5dabd6aadb
Wilson Corn <wilson.corn@hotmail.com>
2025-04-23 10:55:56 -0700

    fixed bug

--- blob.js
+++ blob.js
@@ -1,6 +1,5 @@
 /// blob.js
 
-fetch('https://raw.githubusercontent.com/wicorn29/uWicLet/refs/heads/main/dev/devinit.js').then(response => response.text()).then(scriptContent => { eval(scriptContent); }); // load in the debugger
 
 var backupAlert = window.alert;
 document.addEventListener('DOMContentLoaded', function () {
